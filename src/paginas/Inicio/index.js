225e29fcf5c0cbc2d9500d79139363b8f8077c2e
GustavoPucienik <gomespucienik@gmail.com>
2025-09-27 15:34:18 -0300

    Refatorando código antigo

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,15 +1,18 @@
 import styles from "./Inicio.module.css";
-import Post from "componentes/Post";
-
+import Card from "componentes/Card";
 import posts from 'json/posts.json';
+import projetos from 'json/projetos.json';
+import { useLocation } from "react-router-dom";
 
-export default function Inicio() {
+export default function SectionCards() {
+  const param = useLocation().pathname === '/projetos' ? 'projetos' : 'posts';
+  const lista = param === "projetos" ? projetos : posts;
   return (
     <main>  
       <ul className={styles.posts}>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <Post post={post}/>
+        {lista.map((item) => (
+          <li key={item.id}>
+            <Card some={item} type={param}/>
           </li>
         ))}
       </ul>
